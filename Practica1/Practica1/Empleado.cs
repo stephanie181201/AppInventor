4f262f5ad6c12ee0b12fa299507bf215283e2655
stephanie181201 <jaguilargodine@gmail.com>
2022-06-07 15:39:42 -0500

    Commit inicial

---
+++ Practica1/Practica1/Empleado.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practica1
+{
+    class Empleado
+    {
+         private string numsecsoc, nombre, primerApellido;
+        private byte edad;
+
+        public string Numsecsoc {
+            get => numsecsoc; 
+            set => numsecsoc = value;
+        }
+        public string Nombre {
+            get => nombre;
+            set => nombre = value;
+        }
+        public string PrimerApellido {
+            get => primerApellido;
+            set => primerApellido = value;
+        }
+        public byte Edad {
+            get => edad;
+            set => edad = value;
+        }
+    }
+    
+}
---
+++ Practica1/Practica1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Practica1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //objeto
+            Empleado emp1 = new Empleado();
+
+            //leer datos por teclado
+            emp1.Nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese el Nombre");
+            emp1.Numsecsoc = Console.ReadLine();
+            Console.WriteLine("Ingrese el Numero de seguro");
+            emp1.PrimerApellido = Console.ReadLine();
+            Console.WriteLine("Ingrese el apellido");
+            emp1.Edad = Byte.Parse(Console.ReadLine());
+            Console.WriteLine("Ingresa la edad");
+
+            Console.WriteLine(emp1.Nombre);
+            Console.WriteLine(emp1.Numsecsoc);
+        }
+    }
+}
